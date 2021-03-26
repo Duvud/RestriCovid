78d38b8d5c9fc3a047832d5c4b343ab0da20b018
Aritz <aritzbello@live.com>
2021-03-26 12:28:50 +0100

    Buah este commit está piola xd

---
+++ RestriCovidBL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RestriCovidBL
+{
+    public class Class1
+    {
+    }
+}
---
+++ RestriCovidData/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RestriCovidData
+{
+    public class Class1
+    {
+    }
+}
