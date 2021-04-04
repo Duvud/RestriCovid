8e30254c4010ca8f649da1481733f352dc07c667
David <davidbrunalvarez@gmail.com>
2021-04-04 21:27:57 +0200

    Alerta cuando se carguen los datos y validador de codigo postal con alerta implementados

--- restricovid/src/RestriCovid.js
+++ restricovid/src/RestriCovid.js
@@ -1,17 +1,57 @@
 import logo from './res/img/RestriCovid.png';
 import styles from './RestriCovid.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Input, Button, List, Divider } from 'antd';
+import { Input, Button, List, Divider, Alert } from 'antd';
 import React, { useState, useEffect } from "react";
 
 //Función principal donde almacenaremos toda la lógica y los componentes
 function RestriCovid() {
   //Hooks useState
   const [codigoPostal, guardarCodigoPostal] = useState("");
+  const [boolCodigoValidado, guardarCodigoValidado] = useState(null);
   const [objRestricciones, guardarObjRestricciones] = useState({});
   const [boolMostrarRestricciones, guardarBoolMostrarRestricciones] = useState(
     false
   );
+
+  /**
+   * Esta función valida el código postal asegurandose de que hay
+   * 5 numeros del 0-9 y que no haya ningún otro caracter
+   * 
+   * @param {str} codigoPostal 
+   * @returns false | true
+   */
+  const ValidarCodigoPostal = codigoPostal => {
+    return /[0-9]{5}/.test(codigoPostal) && /[^0-9]/.test(codigoPostal) === false ?  true : false
+  }
+
+  //Está función cargará los datos haciendo una petición a la API y llama al validador de código postal
+  const MostrarRestricciones = codigoPostal => {
+    if(ValidarCodigoPostal(codigoPostal) === false){
+      guardarCodigoValidado(false);
+    }else{
+      guardarCodigoValidado(true);
+      let datos = {
+        codigo: codigoPostal,
+        arRestricciones: [
+          {
+            id: 1,
+            zona: "Lezo",
+            restriccion:
+              "No se puede salir de casa, solo para comprar alimentos indispensables",
+          },
+          {
+            id: 2,
+            zona: "San Sebastian",
+            restriccion:
+              "No se puede salir de casa, solo para comprar alimentos indispensables",
+          },
+        ],
+      };
+      guardarObjRestricciones(datos);
+      console.log("objeto restricciones => ", objRestricciones);
+    }
+  }
 
   /**
    * Este useEffect activa la generación de tabla de restricciones
@@ -22,30 +62,6 @@ function RestriCovid() {
       guardarBoolMostrarRestricciones(true);
     }
   }, [objRestricciones]);
-
-  //Está función cargara los datos haciendo una petición a la API
-  function MostrarRestricciones(codigoPostal) {
-    alert(codigoPostal);
-    let datos = {
-      codigo: codigoPostal,
-      arRestricciones: [
-        {
-          id: 1,
-          zona: "Lezo",
-          restriccion:
-            "No se puede salir de casa, solo para comprar alimentos indispensables",
-        },
-        {
-          id: 2,
-          zona: "San Sebastian",
-          restriccion:
-            "No se puede salir de casa, solo para comprar alimentos indispensables",
-        },
-      ],
-    };
-    guardarObjRestricciones(datos);
-    console.log("objeto restricciones => ", objRestricciones);
-  }
 
   //Componente que prepara el logo centrado en la parte superior de la página
   function Logo(props) {
@@ -100,7 +116,7 @@ function RestriCovid() {
           />
         </div>
 
-        <div className={`mt-5`}>
+        <div className={"mt-5"}>
           <Button
             className={"mb-5"}
             id="btnMostrarRestricciones"
@@ -110,10 +126,19 @@ function RestriCovid() {
           </Button>
         </div>
       </div>
-      {boolMostrarRestricciones !== false ? (
+      {boolMostrarRestricciones === true && boolCodigoValidado === true ? (
         <>
+          <div className={`m-auto col-6`}>
+            <Alert type="success" message="Restricciones cargadas correctamente" />
+          </div>
           <div>
             <Restricciones datos={objRestricciones}></Restricciones>
+          </div>
+        </>
+      ) : boolCodigoValidado === false && codigoPostal !== "" ? (
+        <>
+          <div className={`m-auto col-6`}>
+            <Alert type="error" message="El codigo postal introducido no es correcto" />
           </div>
         </>
       ) : null}
